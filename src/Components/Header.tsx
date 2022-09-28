cc32ab10b056994a85290746d5c7467589b10cc6
Vipershroom <undertalefan812@gmail.com>
2022-09-28 17:38:35 -0400

    Import react

---
+++ src/Components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from '../images/eight_ball.png'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <img src={Image} alt="eight ball" />
+    </div>
+  )
+}
+
+export default Header
