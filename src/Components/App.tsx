957719ba2764928d66e65a66e594c5a706e980d6
Vipershroom <undertalefan812@gmail.com>
2022-10-15 02:57:39 -0400

    Centered button & finished form state

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Header from './Header';
 import './styles/App.scss'
 
+let responses = [
+  "It is certain.",
+  "It is decidedly so.",
+  "Without a doubt.",
+  "Yes definitely.",
+  "You may rely on it.",
+  "As I see it, yes.",
+  "Most likely.",
+  "Outlook good.",
+  "Yes.",
+  "Signs point to yes.",
+  "Reply hazy, try again.",
+  "Ask again later.",
+  "Better not tell you now.",
+  "Cannot predict now.",
+  "Concentrate and ask again.",
+  "Don't count on it.",
+  "My reply is no.",
+  "My sources say no.",
+  "Outlook not so good.",
+  "Very doubtful.",
+  ]
+
+function randRes(responses: Array<string>) {
+  let randNum = Math.floor(Math.random() * responses.length)
+  return responses[randNum]
+}
+
 function App() {
+
+  const [state, setState] = useState("")
+  const inputRef = useRef(null)
+
   return (
     <div className='main'>
       <Header />
@@ -12,8 +44,16 @@ function App() {
       <div className="prompt">
         <h2>Please ask a question!</h2>
         <div className="field">
-          <input type="text" name="" id="inp" />
+          <input ref={inputRef} type="text" name="" id="inp" />
+          <button onClick={() => {
+            if (inputRef.current!['value'] === "") {
+              return
+            } else {
+              setState(randRes(responses))
+            }
+          }}>Submit</button>
         </div>
+        <h2>Answer: {state}</h2>
             </div>
       </div>
   );
